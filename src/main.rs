4e4e1f9b4f4aa83a6d8d0ee9ae66edad9dbf3f8b
Max Jacobson <max@hardscrabble.net>
2016-06-05 18:17:42 -0400

    cargo crate for printing a random nice emoji
    I use this in my prompt -- it was in ruby and sliiiiiightly slow so I
    thought I'd try to make it faster

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+extern crate rand;
+use rand::distributions::{IndependentSample, Range};
+
+fn main() {
+    // cool, friendly emoji that look fine against a black terminal background
+    let list = vec!["🐖", "😅", "🌸"];
+    let between = Range::new(0, list.len());
+    let mut rng = rand::thread_rng();
+    let index = between.ind_sample(&mut rng);
+    let emoji = list[index];
+    print!("{}", emoji);
+}
