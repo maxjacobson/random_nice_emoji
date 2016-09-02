573c79f601cc3cc35db28e28ddece2212b8a9e09
Max Jacobson <max@hardscrabble.net>
2016-09-01 23:58:05 -0400

    factor out a sample method

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 extern crate rand;
 use rand::distributions::{IndependentSample, Range};
 
+fn sample(list: Vec<&str>) -> &str {
+    let between = Range::new(0, list.len());
+    let mut rng = rand::thread_rng();
+    let index = between.ind_sample(&mut rng);
+    let el = list[index];
+    return el;
+}
+
 fn main() {
     // cool, friendly emoji that look fine against a black terminal background
     let list = vec!["🐖", "😅", "🌸", "🐙", "🎑", "🖌", "☕", "📊", "🐋", "🌈",
                     "✨"];
-    let between = Range::new(0, list.len());
-    let mut rng = rand::thread_rng();
-    let index = between.ind_sample(&mut rng);
-    let emoji = list[index];
+    let emoji = sample(list);
     print!("{}", emoji);
 }
